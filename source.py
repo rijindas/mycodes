dcd1959a0080ecbf4e9f3414da70a9d8e4d48ed2
rijindas <riji.n@ictkerala.org>
2019-09-16 16:10:29 +0530

    test

---
+++ source.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+
+data = pd.read_csv("C:/Users/Riji/Desktop/new_customers.csv",skiprows = 1, header = None)
+pca = PCA()
+Data = pca.fit_transform(data)
+data = pca.transform(Data)
+print(data)
